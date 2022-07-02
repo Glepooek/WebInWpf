eec0237f0d4143d343adecd3154da605484246d0
安玉 <glepooek@outlook.com>
2022-07-02 23:03:24 +0800

    feat: webview2加载web

--- WebInWpf/WebInWpf.WebView2/MainWindow.xaml.cs
+++ WebInWpf/WebInWpf.WebView2/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +24,28 @@ namespace WebInWpf.WebView2
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += async (sender, args) =>
+            {
+                await this.webview.EnsureCoreWebView2Async();
+            };
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window();
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.Owner = this;
+            window.Owner.Hide();
+            Microsoft.Web.WebView2.Wpf.WebView2 webView2 = new Microsoft.Web.WebView2.Wpf.WebView2()
+            {
+                Source = new Uri("https://www.bing.com")
+            };
+            window.Content = webView2;
+            window.Show();
+            window.Closed += (sender, args) =>
+            {
+                window.Owner.Show();
+            };
         }
     }
 }
